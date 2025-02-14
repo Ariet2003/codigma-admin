89e8b7857f960ba85f15943fcca6a5b9e697f123
Ariet2003 <amanbekovariet405@gmail.com>
2025-02-14 22:25:05 +0600

    Styled UI to match Codigma's design

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ import streamlit as st
 from db import get_user_by_email
 from pages.admin_dashboard import show_admin_dashboard
 
+custom_css = """
+<style>
+/* Стили для кнопок при наведении */
+div.stButton > button:hover {
+    background-color: #1F2A3D !important;
+    color: #FFFFFF !important;
+}
+</style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
+
+hide_sidebar_style = """
+    <style>
+    [data-testid="stSidebar"] { display: none; }
+    [data-testid="stSidebarCollapsedControl"] { display: none; }
+    </style>
+"""
+st.markdown(hide_sidebar_style, unsafe_allow_html=True)
 
 def main():
-    hide_sidebar_style = """
-        <style>
-        [data-testid="stSidebar"] { display: none; }
-        [data-testid="stSidebarCollapsedControl"] { display: none; }
-        </style>
-    """
-    st.markdown(hide_sidebar_style, unsafe_allow_html=True)
+
 
     # Если пользователь не аутентифицирован, показываем кнопку для входа
     if not st.experimental_user.is_logged_in:
